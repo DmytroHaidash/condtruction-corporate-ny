993896562bdb59bf7213b017d537a711e3bc337f
Dmytro Haidash <dmyto.haidash@gmail.com>
2023-02-26 15:38:09 -0600

    updates

--- resources/views/partials/app/layout/footer.blade.php
+++ resources/views/partials/app/layout/footer.blade.php
@@ -4,36 +4,56 @@
         <div class="row justify-content-between mb-4">
             <div class="footer-item footer-copyright">
             </div>
-            <ul class="social-list">
+            <ul class="social-list ">
                 <li>
-                    <a href="{{ route('app.home') }}">
+                    <a href="{{ route('app.home') }}" class="footer-menu">
                         @lang('pages.header.home')
                     </a>
                 </li>
                 <li>
-                    <a href="{{route('app.services.index')}}">
+                    <a href="{{route('app.services.index')}}" class="footer-menu">
                         @lang('pages.header.service')
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('app.pages.about') }}">
+                    <a href="{{ route('app.pages.about') }}" class="footer-menu">
                         @lang('pages.header.about')
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('app.articles.index') }}">
+                    <a href="{{ route('app.articles.index') }}" class="footer-menu">
                         @lang('pages.header.blog')
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('app.pages.contacts') }}">
+                    <a href="{{ route('app.pages.contacts') }}" class="footer-menu">
                         @lang('pages.header.contacts')
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('app.pages.privacy') }}">
+                    <a href="{{ route('app.pages.privacy') }}" class="footer-menu">
                         @lang('pages.header.privacy')
                     </a>
+                </li>
+            </ul>
+            <div class="footer-item mt-2 mt-sm-0">
+            </div>
+        </div>
+        <div class="row justify-content-between mb-4">
+            <div class="footer-item footer-copyright">
+            </div>
+            <ul class="social-list">
+                <li>
+                    <p>License:</p>
+                </li>
+                <li>
+                    <p>NYS LIC # 16000079372</p>
+                </li>
+                <li>
+                    <p>NJS LIC # 24GI00191800</p>
+                </li>
+                <li>
+                    <p>MOLD ASSESSOR NYS DOL CERT# MA01425</p>
                 </li>
             </ul>
             <div class="footer-item mt-2 mt-sm-0">
