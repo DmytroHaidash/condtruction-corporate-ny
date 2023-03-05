6fed333582e1cb612f0abbfa971ebb3990afdf51
Dmytro Haidash <dmyto.haidash@gmail.com>
2023-03-05 15:23:19 -0600

    changes

--- app/Http/Controllers/Front/FeedbackController.php
+++ app/Http/Controllers/Front/FeedbackController.php
@@ -2,19 +2,22 @@
 
 namespace App\Http\Controllers\Front;
 
+use App\Mail\SendFeedback;
 use App\Models\Feedback;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
 
 class FeedbackController extends Controller
 {
     public function store(Request $request)
     {
-        Feedback::create([
+        $feedback = Feedback::create([
             'name' => $request->input('name'),
             'phone' => $request->input('phone'),
             'email' => $request->input('email'),
         ]);
+        Mail::send(new SendFeedback($feedback));
         return \view('app.pages.thanks');
     }
 }
